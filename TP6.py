b8cb5e506b10d212f3ea5f1215977ee1c2bfb9ca
Alan Hryniewicz <alan.hryniewicz@valtech.com>
2018-05-17 16:53:06 -0300

    Corregidos errores varios de compilación, ahora toma N por parametro

--- TP6.py
+++ TP6.py
@@ -1,16 +1,25 @@
+# encoding: utf-8
 import sys
 
-def inicializarColas(N)
+# Get the total number of args passed to the program
+totalArgs = len(sys.argv)
+ 
+if totalArgs != 2:
+	exit(u"Esta simulación recibe una variable de control N (Cantidad de puestos de atención)")
+
+N = int(sys.argv[1])
+
+def inicializarColas(N):
 	for elem in range(N):
-		TPS[elem] = sys.maxint
-		ITO[elem] = 0
-		STO[elem] = 0
+		TPS.append(sys.maxint)
+		ITO.append(0)
+		STO.append(0)
 
 def menorTPS():
 	min = sys.maxint
 	minIndex = 0
 	for index in range(len(TPS)):
-		if TPS[index] < min
+		if TPS[index] < min:
 			min = TPS[index]
 			minIndex = index 
 	return minIndex
@@ -19,58 +28,66 @@ def HVTPS():
 	HV = sys.maxint
 	maxIndex = 0
 	for index in range(len(TPS)):
-		if TPS[index] = HV
+		if TPS[index] == HV:
 			maxIndex = index
 			break
 	return maxIndex
 
-N = 0
+def generarTA():
+	return 1
+
+def generarIA():
+	return 1
+
+def calcularArrep():
+	return 1
+
+MENORTPS = 0
+CANTARREP =0
 TPLL = 0
 STS = 0
 NS = 0
-CLL = 0
+CLL =0
 T = 0
 TF = 9999999999999
 IA = 0
 TA = 0
-ARREP = FALSE
-CANTARREP = 0
+ARREP = False
 TPS = []
 ITO = []
 STO = []
+
 inicializarColas(N)
-MENORTPS = 0
 
-
-while ((T < TF) or (T >= TF && NS > 0)):
+while ((T < TF) or (T >= TF and NS > 0)):
 	MENORTPS = menorTPS()
-	if TPLL <= TPS[MENORTPS]
+	if TPLL <= TPS[MENORTPS]:
 		STS += ((TPLL - T) * NS)
 		T = TPLL
 		IA = generarIA()
 		TPLL = T + IA		
 		ARREP = calcularArrep()
 		if ARREP:
-			CANTARREP++
+			CANTARREP += 1
 			continue
-		NS++
-		CLL++
-		if NS < N
+		NS += 1
+		CLL += 1
+		if NS < N:
 			TA = generarTA()
 			HVTPSindex = HVTPS()
 			TPS[HVTPSindex] = T + TA
 			STO[HVTPSindex] += (T - ITO(HVTPSindex))
-	else
+	else:
 		STS += ((TPS[MENORTPS] - T) * NS)
 		T = TPS[MENORTPS]
-		NS--
-		if NS >= N
+		NS -= 1
+		if NS >= N:
 			TA = generarTA()
 			TPS[MENORTPS] = T + TA
-		else
+		else:
 			ITO[MENORTPS] = T
 			TPS[MENORTPS] = sys.maxint
-	if T >= TF && NS > 0
+	if T >= TF and NS > 0:
 		TPLL = sys.maxint
 
 #calcular los valores y hacer rutinas de calcularArrep y generarTA Y generarIA.
